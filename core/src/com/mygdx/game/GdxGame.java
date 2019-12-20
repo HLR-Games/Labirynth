defdfddbeb18f5faa89693b581ef39a8c573c5ae
TheScreamer74 <blakilleur@gmail.com>
2019-12-20 02:50:49 +0100

    20/12/2019 2:50 Starting the loot module added the switch between open and close.

--- core/src/com/mygdx/game/GdxGame.java
+++ core/src/com/mygdx/game/GdxGame.java
@@ -3,8 +3,10 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.Enum.Screen;
 
@@ -14,13 +16,51 @@ public class GdxGame extends ApplicationAdapter {
 
 	public int appState = Screen.MAIN_MENU;
 
+	public Sprite closeChest;
+    public Sprite openChest;
+
+
+    public float largeur_ecran;
+    public float hauteur_ecran;
+    public float xPosCloseChest;
+    public float yPosCloseChest;
+    public float xPosOpenChest;
+    public float yPosOpenChest;
+
+    // FONCTIONS DE CALCUL DE RESOLUTION ===========================================================
+    private float xUnite(float x)
+    {
+        return x*largeur_ecran/480f;
+    }
+
+    private float yUnite(float y)
+    {
+        return y*hauteur_ecran/320;
+    }
+
 
 	// INITIALISATION ==============================================================================
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
-	}
+        largeur_ecran = Gdx.graphics.getWidth();
+        hauteur_ecran = Gdx.graphics.getHeight();
+
+		closeChest = new Sprite(new Texture(Gdx.files.internal("closechest.png")));
+		openChest = new Sprite(new Texture(Gdx.files.internal("openchest.png")));
+
+		closeChest.setSize(xUnite(250), yUnite(200));
+		openChest.setSize(xUnite(250), yUnite(200));
+
+		xPosCloseChest = xUnite(176);
+		yPosCloseChest = yUnite(150);
+
+        xPosOpenChest = xUnite(176);
+        yPosOpenChest = yUnite(150);
+
+
+    }
 
 
 	// MAIN ========================================================================================
@@ -88,9 +128,15 @@ public class GdxGame extends ApplicationAdapter {
 	// Screens -------------------------------------------------------------------------------------
 	public void gameScreenDisp(){
 
+        openChest.setPosition(xPosOpenChest, yPosOpenChest);
+        openChest.draw(batch);
+
 	}
 
 	public void mainMenuDisp(){
+
+        closeChest.setPosition(xPosCloseChest, yPosCloseChest);
+        closeChest.draw(batch);
 
 	}
 
@@ -109,6 +155,51 @@ public class GdxGame extends ApplicationAdapter {
     }
 
     public void mainMenuInput(){
+
+        Gdx.input.setInputProcessor(new InputProcessor() {
+            @Override
+            public boolean keyDown(int keycode) {
+                return false;
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                return false;
+            }
+
+            @Override
+            public boolean keyTyped(char character) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                if (screenX > xUnite(180) && screenX < xUnite(430) && screenY > yUnite(115) && screenY < yUnite(300)) {
+                    appState = Screen.GAME_SCREEN;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDragged(int screenX, int screenY, int pointer) {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved(int screenX, int screenY) {
+                return false;
+            }
+
+            @Override
+            public boolean scrolled(int amount) {
+                return false;
+            }
+        });
 
     }
 
